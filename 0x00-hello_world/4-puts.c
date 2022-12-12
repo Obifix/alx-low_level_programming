b350f1a0a1c13839c39ec05c9be7e8a2441fa4a9
Obifix <anorueobinna316@gmail.com>
2022-12-12 12:21:04 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+
+
+/**
+ *
+ * * main .main block
+ *
+ * *return: 0
+ *
+ * */
+
+int main(void)
+
+{
+
+	           puts(*\"programing is like develop a multilingual puzzle");
+
+		              return (0);
+
+}
